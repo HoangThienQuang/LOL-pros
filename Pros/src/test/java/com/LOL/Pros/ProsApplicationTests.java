701eb9afae7e7adc2c6085e1e86dff98b5703019
HoangThienQuang <quang2.11.1999@gmail.com>
2024-10-08 01:39:33 +0700

    create base LOL pros project

---
+++ Pros/src/test/java/com/LOL/Pros/ProsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LOL.Pros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
