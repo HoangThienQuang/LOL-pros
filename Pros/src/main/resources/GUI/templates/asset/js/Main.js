1c2794c505f420856cbd9c0278b2108585a18491
HoangThienQuang <quang2.11.1999@gmail.com>
2024-11-07 17:10:29 +0700

    update UI, modify js to call page via content tag in html

--- Pros/src/main/resources/GUI/templates/asset/js/Main.js
+++ Pros/src/main/resources/GUI/templates/asset/js/Main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {//thêm event đợi DOM load xong
     document.querySelectorAll('nav ul li a').forEach(function(link) {//chọn tất cả các phần tử nav ul li a sau đó thực hiện function link cho từng phần tử
-        link.addEventListener('click', handleNavClick);//link thực hiện thao tác lick phần tử và thực thi hàm handleNavClick
+        link.addEventListener('click', homepageNav);//link thực hiện thao tác lick phần tử và thực thi hàm handleNavClick
     });
 
     // Tải trang chủ mặc định khi tải trang
@@ -10,3 +10,54 @@ document.addEventListener('DOMContentLoaded', function() {//thêm event đợi D
             document.getElementById('main-content').innerHTML = html;
         });
 });
+
+function homepageNav(event)
+{
+    event.preventDefault();
+    const page = event.target.getAttribute('data-page');
+    fetch(page + '.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('main-content').innerHTML = html;
+        })
+        .catch(error => 
+        {
+            console.error('Error when loading page: ', error);
+        }
+        )
+}
+//--------------------------------------------------------------------------------
+
+//----------------------------------function--------------------------------------
+// function handleNavClick(event) {
+//     event.preventDefault();
+//     const page = event.target.getAttribute('data-page');
+//     fetch(page + '.html')
+//         .then(response => response.text())
+//         .then(html => {
+//             document.getElementById('main-content').innerHTML = html;
+//                 setupSubPageLinks();
+//         })
+//         .catch(error => {
+//             console.error('Error loading page:', error);
+//         });
+// }
+
+// function setupSubPageLinks() {
+//     const subNavLink = document.querySelectorAll('#navbarNav .nav-link');
+//     subNavLink.forEach(function(link) {
+//         link.addEventListener('click', function(event) {
+//             event.preventDefault();
+//             const subPage = event.target.getAttribute('data-page');
+//             fetch(subPage + '.html')
+//                 .then(response => response.text())
+//                 .then(html => {
+//                     document.getElementById('sub-content').innerHTML = html;
+//                 })
+//                 .catch(error => {
+//                     console.error('Error loading sub-page:', error);
+//                     document.getElementById('sub-content').innerHTML = '<p>Error loading sub-page.</p>';
+//                 });
+//         });
+//     });
+// }
