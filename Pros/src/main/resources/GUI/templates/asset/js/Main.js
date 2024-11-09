e2bcc241a9b5b00e47a8b6ac0a843652f0dd6f74
HoangThienQuang <quang2.11.1999@gmail.com>
2024-11-09 23:18:37 +0700

    update load date to player and team page, split to 2 flow to prevent bug : get wrong id, tag, attribute

--- Pros/src/main/resources/GUI/templates/asset/js/Main.js
+++ Pros/src/main/resources/GUI/templates/asset/js/Main.js
@@ -24,6 +24,8 @@ function loadPage(page) {
             // }
             if(page === 'player')
                 loadAllPlayer();
+            if(page === 'team')
+                loadAllTeam();
         })
         .catch(error => {
             console.error('Error when loading page: ', error);
@@ -97,7 +99,7 @@ function loadAllPlayer()
         { "ingameName": "Perkz", "National": "Croatia", "Team": "Cloud9" },
         { "ingameName": "Doublelift", "National": "USA", "Team": "TSM" }
     ];
-    displayPage(data, currentPage);
+    displayPlayerTable(data, currentPage);
     setupPaginationEvents();
     updatePagination();
     // fetch('player-data-api-endpoint')
@@ -129,13 +131,13 @@ function setupPaginationEvents()
             else if (!isNaN(text)) {
                 currentPage = Number(text);    
             }
-            displayPage(data,currentPage);
+            displayPlayerTable(data,currentPage);
             updatePagination();
         });
     });
 }
 
-function displayPage(data, page) {
+function displayPlayerTable(data, page) {
     const startIndex = (page - 1) * rowsPerPage;
     const endIndex = startIndex + rowsPerPage;
     const currentPageData = data.slice(startIndex, endIndex);
@@ -158,7 +160,8 @@ function displayPage(data, page) {
 
 function updatePagination()
 {
-    const pagination = document.getElementById('pagination');
+    //const pagination2 = document.getElementById('pagination');
+    const pagination = document.querySelector('[pagination-page="player-pagination"]')
     const totalPages = Math.ceil(data.length / rowsPerPage);
 
     if(totalPages <= 1)
@@ -198,4 +201,144 @@ function updatePagination()
 
     pagination.innerHTML = paginationHTML;
     setupPaginationEvents()
+}
+
+//----------------------------------team table function --------------------------------------
+let teamCurrentSheet = 1;
+const teamMaxRow = 5;
+let teamData = [];
+function loadAllTeam()
+{
+    teamData = [
+        { "TeamName": "Levi", "Region": "VietNam", "Sponsor": "GAM" },
+        { "TeamName": "Bang", "Region": "Korea", "Sponsor": "T1" },
+        { "TeamName": "Faker", "Region": "Korea", "Sponsor": "T1" },
+        { "TeamName": "Levi", "Region": "VietNam", "Sponsor": "GAM" },
+        { "TeamName": "Bang", "Region": "Korea", "Sponsor": "T1" },
+        { "TeamName": "Faker", "Region": "Korea", "Sponsor": "T1" },
+        { "TeamName": "Uzi", "Region": "China", "Sponsor": "RNG" },
+        { "TeamName": "Caps", "Region": "Denmark", "Sponsor": "G2" },
+        { "TeamName": "Perkz", "Region": "Croatia", "Sponsor": "Cloud9" },
+        { "TeamName": "Levi", "Region": "VietNam", "Sponsor": "GAM" },
+        { "TeamName": "Bang", "Region": "Korea", "Sponsor": "T1" },
+        { "TeamName": "Faker", "Region": "Korea", "Sponsor": "T1" },
+        { "TeamName": "Levi", "Region": "VietNam", "Sponsor": "GAM" },
+        { "TeamName": "Bang", "Region": "Korea", "Sponsor": "T1" },
+        { "TeamName": "Faker", "Region": "Korea", "Sponsor": "T1" },
+        { "TeamName": "Uzi", "Region": "China", "Sponsor": "RNG" },
+        { "TeamName": "Caps", "Region": "Denmark", "Sponsor": "G2" },
+        { "TeamName": "Perkz", "Region": "Croatia", "Sponsor": "Cloud9" },
+        { "TeamName": "Levi", "Region": "VietNam", "Sponsor": "GAM" },
+        { "TeamName": "Bang", "Region": "Korea", "Sponsor": "T1" },
+        { "TeamName": "Faker", "Region": "Korea", "Sponsor": "T1" },
+        { "TeamName": "Levi", "Region": "VietNam", "Sponsor": "GAM" },
+        { "TeamName": "Bang", "Region": "Korea", "Sponsor": "T1" },
+        { "TeamName": "Faker", "Region": "Korea", "Sponsor": "T1" },
+        { "TeamName": "Uzi", "Region": "China", "Sponsor": "RNG" },
+        { "TeamName": "Caps", "Region": "Denmark", "Sponsor": "G2" },
+        { "TeamName": "Perkz", "Region": "Croatia", "Sponsor": "Cloud9" }
+    ];
+    displayTeamTable(teamData, teamCurrentSheet);
+    setupTeamPaginationEvents();
+    updateTeamPagination();
+    // fetch('player-data-api-endpoint')
+    // .then(response => response.json())
+    // .then(data => {
+    //      const tbody = document.getElementById('player-table-body');
+    //      // Đổ dữ liệu vào bảng
+    //          displayTableData(data, currentPage);
+    //         })
+    //         .catch(error => { console.error('Error fetching player data:', error); });
+}
+
+function displayTeamTable(teamData, teamCurrentSheet) {
+    const startIndex = (teamCurrentSheet - 1) * rowsPerPage;
+    const endIndex = startIndex + rowsPerPage;
+    const currentPageData = teamData.slice(startIndex, endIndex);
+
+    
+    const tbody = document.getElementById('team-table-body');
+    tbody.innerHTML = '';//xóa dữ liệu cũ trước khi thêm dữ liệu mới
+
+    currentPageData.forEach((item, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <th scope="row">${startIndex + index + 1}</th>
+            <td>${item.TeamName}</td>
+            <td>${item.Region}</td>
+            <td>${item.Sponsor}</td>
+        `;
+        tbody.appendChild(row);
+    });
+}
+
+function setupTeamPaginationEvents()
+{
+    //const pagination = document.getElementById('pagination');//lấy phần tử có id pagination
+    const pagination = document.querySelector('[pagination-page="team-pagination"]')
+    const pageLinks = pagination.querySelectorAll('.page-link');//lấy tất cả các phần tử có thuộc tính page-link của pagination
+
+    pageLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const text = link.innerText;
+            if (text === '«' && teamCurrentSheet > 1){
+                teamCurrentSheet--;
+            }
+            else if (text === '»'){
+                const totalPages = Math.ceil(teamData.length / rowsPerPage);
+                if (teamCurrentSheet < totalPages) teamCurrentSheet++;
+            }
+            else if (!isNaN(text)) {
+                teamCurrentSheet = Number(text);    
+            }
+            displayTeamTable(teamData,teamCurrentSheet);
+            updateTeamPagination();
+        });
+    });
+}
+
+function updateTeamPagination()
+{
+    //const pagination2 = document.getElementById('pagination');
+    const pagination = document.querySelector('[pagination-page="team-pagination"]')
+    const totalPages = Math.ceil(teamData.length / rowsPerPage);
+
+    if(totalPages <= 1)
+    {
+        pagination.style.display = 'none';
+    }
+    else
+    {
+        pagination.style.display = 'flex';
+    }
+
+    let paginationHTML = `
+    <li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+        </a>
+    </li>`;
+
+    if (totalPages <= 4) {
+        for (let i = 1; i <= totalPages; i++) {
+            paginationHTML += `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`;
+        }
+    } else {
+        paginationHTML += `<li class="page-item"><a class="page-link" href="#">1</a></li>
+                           <li class="page-item"><a class="page-link" href="#">2</a></li>
+                           <li class="page-item"><a class="page-link" href="#">3</a></li>
+                           <li class="page-item">...</li>
+                           <li class="page-item"><a class="page-link" href="#">${totalPages}</a></li>`;
+    }
+
+    paginationHTML += `
+        <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+            </a>
+        </li>`;
+
+    pagination.innerHTML = paginationHTML;
+    setupTeamPaginationEvents()
 }
