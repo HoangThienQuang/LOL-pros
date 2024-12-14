3de016ceae3c1d09cd86b8a3f1180e303bacfe60
HoangThienQuang <quang2.11.1999@gmail.com>
2024-12-14 14:53:13 +0700

    update base prefix

--- Pros/src/main/java/com/LOL/Pros/Entity/Player.java
+++ Pros/src/main/java/com/LOL/Pros/Entity/Player.java
@@ -13,9 +13,20 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Player {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO) // tự động tạo id kiểu tự động tăng
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "entity_seq")// tự động tạo id kiểu chuỗi
+    @SequenceGenerator(name = "entity_seq", sequenceName = "entity_sequence",allocationSize = 1)//tạo chuỗi với giá trị tự động tăng
     @Column(name = "playerId", nullable = false, length = 100)
     private String playerId;
+
+    @Transient // tạo một id không lưu vào DB
+    private String prefixed;
+
+    @PrePersist //trước khi data được lưu vào DB sẽ thực thi hàm dưới để thêm prefix vào ID
+    @PostLoad
+    public void generateID()
+    {
+        this.prefixed = "Player-" + this.playerId;
+    }
 
     @Column(name = "ingameName", nullable = false, length = 100)
     private String ingameName;
--- Pros/src/main/java/com/LOL/Pros/Entity/Team.java
+++ Pros/src/main/java/com/LOL/Pros/Entity/Team.java
@@ -4,13 +4,23 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Getter
 @Setter
 @Entity
 public class Team {
+
+    private static final AtomicLong counter = new AtomicLong(1);//tạo giá trị tự động tăng
+
     @Id
     @Column(name = "teamId", nullable = false, length = 100)
     private String teamId;
+
+    @PrePersist // tạo hàm gọi thêm prefix vào ID trước khi lưu xuống DB
+    public void generateId() {
+        this.teamId = "Team-" + counter.getAndIncrement();
+    }
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "teamCaptain")
